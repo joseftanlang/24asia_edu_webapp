b75121c471eb072a78ec1e2fa11e066533996d61
Josef Tan <tanjosef03@gmail.com>
2026-06-14 02:03:09 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,42 @@ import 'auth_page/firebase_service.dart';
 import 'auth_page/auth_screen.dart';
 import 'auth_page/home_page.dart';
 import 'setting/setting.dart';
+import 'clocking_sys.dart';
 
 void main() {
-  runApp(const MySetting());
+  runApp(const MyApp());
 }
 
-class MySetting extends StatelessWidget {
-  const MySetting({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SettingsScreens();
+    return MaterialApp(
+      title: 'Volunteer App',
+      theme: ThemeData(
+        primaryColor: const Color(0xff1565C0),
+        scaffoldBackgroundColor: const Color(0xffF4F7FC),
+        fontFamily: 'Roboto',
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SettingsScreens(), // Changed to SettingsScreen
+    );
   }
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+// import 'package:flutter/material.dart';
+// import 'auth_page/firebase_service.dart';
+// import 'auth_page/auth_screen.dart';
+// import 'auth_page/home_page.dart';
+// import 'setting/setting.dart';
+// import 'clocking_sys.dart';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Volunteer App',
-//       theme: ThemeData(
-//         primaryColor: const Color(0xff1565C0),
-//         scaffoldBackgroundColor: const Color(0xffF4F7FC),
-//         fontFamily: 'Roboto',
-//         useMaterial3: true,
-//       ),
-//       debugShowCheckedModeBanner: false,
-//       home: StreamBuilder<bool>(
-//         stream: FirebaseService.instance.authStateChanges(),
-//         builder: (context, snapshot) {
-//           if (snapshot.hasData && snapshot.data == true) {
-//             return const HomePage();
-//           }
-//           return const AuthScreen();
-//         },
-//       ),
-//     );
-//   }
+// void main() {
+//   runApp(const MyApp());
 // }
 
-
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 
@@ -59,15 +53,7 @@ class MySetting extends StatelessWidget {
 //         useMaterial3: true,
 //       ),
 //       debugShowCheckedModeBanner: false,
-//       home: StreamBuilder<bool>(
-//         stream: FirebaseService.instance.authStateChanges(),
-//         builder: (context, snapshot) {
-//           if (snapshot.hasData && snapshot.data == true) {
-//             return const HomePage();
-//           }
-//           return const AuthScreen();
-//         },
-//       ),
+//       home: const ClockInOutPage(), // Now ClockInOutPage has MaterialApp as parent
 //     );
 //   }
 // }
